ec9f97af6acd94abc0c9278292dec0bbd9930805
Jorge Arias Argüelles <jorgeariasarguelles@gmail.com>
2022-02-16 18:13:35 -0500

    add/ remember the last path page visited succesfully

--- src/components/login/LoginScreen.js
+++ src/components/login/LoginScreen.js
@@ -18,7 +18,9 @@ export const LoginScreen = ()=>{
     }
     dispatch(action);
 
-    navigate('/marvel', {
+    const lastPath = localStorage.getItem('lastPath') || '/marvel';
+
+    navigate(lastPath, {
       replace: true
     });
 
