7fef10c9b29217bfb974b38eebc7df5bd9c36739
Jorge Arias Argüelles <jorgeariasarguelles@gmail.com>
2022-02-16 18:53:40 -0500

    update/ move assets/imgs inside src folder

--- src/components/hero/HeroCard.js
+++ src/components/hero/HeroCard.js
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom";
 
 export const HeroCard = ( { id, superhero, publisher, alter_ego, first_appearance, characters } ) => {
 
-  const imgPath = `assets/${id}.jpg`;
+  const imgPath = `../../assets/${id}.jpg`;
 
   return (
     <div className="col animate__animated animate__fadeIn">
